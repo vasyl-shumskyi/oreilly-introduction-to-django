67ef78b3bebe46eff7c0f5066f0f1fa5ee7f78b7
Vasyl Shumskyi <vasyl.shumskyi@gmail.com>
2017-01-17 16:25:28 +0300

    Add list_authors method and overwrite save method on Book object

--- books/models.py
+++ books/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.timezone import now
+
 
 class Book(models.Model):
     title = models.CharField(max_length=150)
@@ -10,7 +12,16 @@ class Book(models.Model):
     is_favourite = models.BooleanField(default=False, verbose_name="Favourite?")
 
     def __str__(self):
-        return self.title
+        return "{} by {}".format(self.title, self.list_authors())
+
+    def list_authors(self):
+        return ", ".join([author.name for author in self.authors.all()])
+
+    def save(self, *args, **kwargs):
+        if (self.review and self.date_reviewed is None):
+            self.date_reviewed = now()
+
+        super(Book, self).save(*args, **kwargs)
 
 class Author(models.Model):
     name = models.CharField(max_length=70, help_text="Use pen name, not real name", unique=True)
