7d1e2991abd59dc93cc3011121c882c2e8075cf5
Vasyl Shumskyi <vasyl.shumskyi@gmail.com>
2017-01-18 17:22:24 +0300

    Create custom BookAdmin

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
-
-# Register your models here.
 from books.models import Author, Book
 
+@admin.register(Book) # shortcut for: admin.site.register(Book, BookAdmin)
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Book Details", {"fields": ["title", "authors"]}),
+        ("Review", {"fields": ["is_favourite", "review", "date_reviewed"]}),
+    ]
+
+    readonly_fields = ("date_reviewed",)
+
+    def book_authors(self, obj):
+        return obj.list_authors()
+
+    book_authors.short_description = "Author(s)"
+
+    list_display = ("title", "book_authors", "date_reviewed","is_favourite",)
+    list_editable = ("is_favourite",)
+    list_display_links = ("title", "date_reviewed",)
+    list_filter = ("is_favourite",)
+    search_fields = ("title", "authors__name",)
+
+
+# Register your models here.
 admin.site.register(Author)
-admin.site.register(Book)
+#admin.site.register(Book, BookAdmin)
+
+
