d37683f7d61eff545cbc7e1d05989058c65df318
Vasyl Shumskyi <vasyl.shumskyi@gmail.com>
2017-01-19 20:48:31 +0300

    03_02-Creating Our First Template And Displaying Dynamic Data

--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def list_books(request):
 #   return HttpResponse("We could put anything here")
-    return HttpResponse(request.user.username)
+#   return HttpResponse(request.user.username)
+    return render(request, "list.html")
