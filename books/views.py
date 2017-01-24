1c7699f49687c5bea317920b28e5ca9c54ee58d4
Vasyl Shumskyi <vasyl.shumskyi@gmail.com>
2017-01-24 23:52:10 +0300

    04_02-Saving Form Data

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView, View
 from books.forms import ReviewForm
 from books.models import Author, Book
@@ -63,7 +63,19 @@ def review_book(request, pk):
     Review an individual book
     """
     book = get_object_or_404(Book, pk=pk)
-    form = ReviewForm
+
+    if request.method == 'POST':
+        # Process our form
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            book.is_favourite = form.cleaned_data['is_favourite']
+            book.review = form.cleaned_data['review']
+            book.save()
+
+            return redirect('review-books')
+    else:
+        form = ReviewForm
 
     context = {
         'book': book,
