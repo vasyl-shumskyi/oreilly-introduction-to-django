1f26916f70fc50a9d7d2aadbd24a6a816664695d
Vasyl Shumskyi <vasyl.shumskyi@gmail.com>
2017-01-27 12:58:54 +0300

    05_03-Logging Users In And Out

--- readit/urls.py
+++ readit/urls.py
@@ -16,12 +16,20 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from books.views import (AuthorDetail, AuthorList, BookDetail, CreateAuthor,
                          list_books, ReviewList, review_book)
 
 
 urlpatterns = [
+    #Auth
+    url(r'^logout/$', auth_views.logout, {'next_page':'books'}, name='logout'),
+    url(r'^login/$', auth_views.login, {'template_name':'login.html'}, name='login'),
+
+    #Admin
     url(r'^admin/', include(admin.site.urls)),
+
+    #Custom
     url(r'^$', list_books, name='books'),
     url(r'^authors/$', AuthorList.as_view(), name='authors'),
     url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name='book-detail'),
