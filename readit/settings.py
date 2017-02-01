66e7dd4310eab00784af011fbcdea20cdea6795b
Vasyl Shumskyi <vasyl.shumskyi@gmail.com>
2017-02-01 12:56:53 +0300

    07_02-Setting Up A Continuous Integration Server - Update settings and requirments to support CI server

--- readit/settings.py
+++ readit/settings.py
@@ -35,7 +35,6 @@ else:
     DEBUG = False
 
 
-
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
@@ -98,6 +97,19 @@ if DJANGO_MODE == 'local':
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
+elif DJANGO_MODE == 'staging':
+    DATABASES = {
+        'default':{
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+            'PORT': os.getenv('DB_PORT', '5432'),
         }
     }
 
