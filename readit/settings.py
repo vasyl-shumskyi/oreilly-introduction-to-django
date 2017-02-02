eb2ee44defb29b12bffc7520591acf896cc73055
Vasyl Shumskyi <vasyl.shumskyi@gmail.com>
2017-02-02 14:03:37 +0300

    07_03-Serving Your Application With Heroku - Update settings and requirments to support production server

--- readit/settings.py
+++ readit/settings.py
@@ -113,6 +113,10 @@ elif DJANGO_MODE == 'staging':
         }
     }
 
+elif DJANGO_MOD == 'production':
+    import dj_database_url
+    # Handles DATABASE_URL environment variable
+    DATABASES = {'default': dj_database_url.config()}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -132,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'readit', 'static'),
